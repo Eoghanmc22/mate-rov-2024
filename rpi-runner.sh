7edf758031c45b18930c91f38fa8e9d681e208ef
Eoghanmc22 <eoghanmc@icloud.com>
2023-12-23 12:23:21 -0500

    Make upload script way faster

--- rpi-runner.sh
+++ rpi-runner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Uploading to Raspberry PI"
+echo "Uploading to Raspberry Pi"
 
-rsync -avPz -e ssh ./detect_cameras.sh ./setup_camera.sh ./robot/motor_data.csv ./robot/robot.toml $1 pi@mate.local:~/mate/ &&
+rsync -avP -e ssh ./detect_cameras.sh ./setup_camera.sh ./robot/motor_data.csv ./robot/robot.toml $1 pi@mate.local:~/mate/ &&
   ssh pi@mate.local "sudo pkill --signal SIGINT $(basename $1) && sleep 0.75 ; sudo pkill $(basename $1) ; sudo pkill gst-launch-1.0 ; cd mate && sudo ./$(basename $1)"
