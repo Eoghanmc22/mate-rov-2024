84931de3baf06489c87ed87f82733fdb74718b6a
Eoghanmc22 <eoghanmc@icloud.com>
2024-01-02 05:20:02 -0500

    Dont clobber old file

--- rpi-runner.sh
+++ rpi-runner.sh
@@ -3,4 +3,4 @@
 echo "Uploading to Raspberry Pi"
 
 rsync -avPz -e ssh  ./detect_cameras.sh ./setup_camera.sh ./robot/motor_data.csv ./robot/robot.toml $1 pi@mate.local:~/mate/ &&
-  ssh pi@mate.local "journalctl -u mate --all --follow -n0 & cd ~/mate/ ; mv ./$(basename $1) robot 2>/dev/null ; sudo systemctl restart mate"
+  ssh pi@mate.local "journalctl -u mate --all --follow -n0 & cd ~/mate/ ; sudo systemctl stop mate ; sleep 0.5 ; rm ./mate ; mv ./$(basename $1) ./mate ; sudo systemctl start mate & cp -p mate ./$(basename $1)"
